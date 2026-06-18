8c384f6e282d730a3bd95a27de234d8e7c1ce490
测试获取会话ID <2398365689@qq.com>
2026-06-18 17:00:41 +0800

    添加B站评论爬虫指令 /bili

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from data.plugins.astrbot_plugin_text import bilibili_comment as bili
+
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -20,10 +22,17 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
+    @filter.command("yuhuan")
     async def getuid(self,event: AstrMessageEvent):
-        user_name = event.session_id
+        user_name = event.get_sender_id()
         if user_name == "yuhuan":
-            yield event.plain_result("yuhuan")
+            yield event.plain_result("yuhuan1")
+        else:
+            yield event.plain_result(event.get_sender_id()+'123')
+
+    # @filter.event_message_type(filter.EventMessageType.ALL)
+    # async def jianting(self, event: AstrMessageEvent):
+    #     yield event.plain_result('成功')
 
 
     @filter.command("bilicomment")
